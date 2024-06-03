76970949324bee276503496577cd7190b93156c6
morimana-cuk <mr716604@gmail.com>
2024-06-04 05:40:05 +0700

    update passowrd , laporan alll, laporan filter

---
+++ README.md
@@ -0,0 +1,8 @@
+setalah di clone atau di pull 
+npm install dulu(khusus fresh project) 
+baru npm start
+
+jangan lupa DB harus sudah tersedia 
+db name = capstone
+
+pake laragon kalo mau enak 
--- route/routes.js
+++ route/routes.js
@@ -1,5 +1,7 @@
 const Joi = require("joi/lib")
-const {login, register} = require('../controller/authcontroller') 
+const {login, register, forget_password, update_password} = require('../controller/authcontroller') 
+const { validate } = require("uuid")
+const { GetAllReport,  GetFilteredReports } = require("../controller/reportcontroller")
 
 const routes =[
     {
@@ -28,7 +30,32 @@ const routes =[
                 })
             }
         }
+    },
+    {
+        method:'POST',
+        path:'/update_password',
+        handler: update_password,
+        options:{
+            validate:({
+                payload:Joi.object({
+                    email:Joi.string().email().required(),
+                    old_password:Joi.string().required(),
+                    new_password:Joi.string().required()
+                })
+            })
+        }
+    },
+    {
+        method: 'GET',
+        path: '/laporan/all',
+        handler: GetAllReport,
+    },
+    {
+        method:'GET',
+        path:'/laporan/filter/{kota}/{kecamatan}/{desa?}',
+        handler: GetFilteredReports,
     }
+    
 ]
 
 module.exports = routes
